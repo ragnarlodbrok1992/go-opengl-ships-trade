0594277dd525af9cb318540046918845e1f5f91c
ragnarlodbrok1992 <maraus92@gmail.com>
2023-10-18 22:29:43 +0200

    WIP

--- ships-trade.go
+++ ships-trade.go
@@ -19,6 +19,90 @@ import (
 const WINDOW_WIDTH = 1024
 const WINDOW_HEIGHT = 768
 
+var cubeVertices = []float32{
+	//  X, Y, Z, U, V
+	// Bottom
+	-1.0, -1.0, -1.0, 0.0, 0.0,
+	1.0, -1.0, -1.0, 1.0, 0.0,
+	-1.0, -1.0, 1.0, 0.0, 1.0,
+	1.0, -1.0, -1.0, 1.0, 0.0,
+	1.0, -1.0, 1.0, 1.0, 1.0,
+	-1.0, -1.0, 1.0, 0.0, 1.0,
+
+	// Top
+	-1.0, 1.0, -1.0, 0.0, 0.0,
+	-1.0, 1.0, 1.0, 0.0, 1.0,
+	1.0, 1.0, -1.0, 1.0, 0.0,
+	1.0, 1.0, -1.0, 1.0, 0.0,
+	-1.0, 1.0, 1.0, 0.0, 1.0,
+	1.0, 1.0, 1.0, 1.0, 1.0,
+
+	// Front
+	-1.0, -1.0, 1.0, 1.0, 0.0,
+	1.0, -1.0, 1.0, 0.0, 0.0,
+	-1.0, 1.0, 1.0, 1.0, 1.0,
+	1.0, -1.0, 1.0, 0.0, 0.0,
+	1.0, 1.0, 1.0, 0.0, 1.0,
+	-1.0, 1.0, 1.0, 1.0, 1.0,
+
+	// Back
+	-1.0, -1.0, -1.0, 0.0, 0.0,
+	-1.0, 1.0, -1.0, 0.0, 1.0,
+	1.0, -1.0, -1.0, 1.0, 0.0,
+	1.0, -1.0, -1.0, 1.0, 0.0,
+	-1.0, 1.0, -1.0, 0.0, 1.0,
+	1.0, 1.0, -1.0, 1.0, 1.0,
+
+	// Left
+	-1.0, -1.0, 1.0, 0.0, 1.0,
+	-1.0, 1.0, -1.0, 1.0, 0.0,
+	-1.0, -1.0, -1.0, 0.0, 0.0,
+	-1.0, -1.0, 1.0, 0.0, 1.0,
+	-1.0, 1.0, 1.0, 1.0, 1.0,
+	-1.0, 1.0, -1.0, 1.0, 0.0,
+
+	// Right
+	1.0, -1.0, 1.0, 1.0, 1.0,
+	1.0, -1.0, -1.0, 1.0, 0.0,
+	1.0, 1.0, -1.0, 0.0, 0.0,
+	1.0, -1.0, 1.0, 1.0, 1.0,
+	1.0, 1.0, -1.0, 0.0, 0.0,
+	1.0, 1.0, 1.0, 0.0, 1.0,
+}
+
+
+var vertexShader = `
+  #version 330
+
+  uniform mat4 projection;
+  uniform mat4 camera;
+  uniform mat4 model;
+
+  in vec3 vert;
+  in vec2 vertTexCoord;
+
+  out vec2 fragTextCoord;
+
+  void main {
+    fragTexCoord = vertTexCoord;
+    gl_Position = projection * camera * model * vec4(vert, 1);
+  }
+` + "\x00"
+
+var fragmentShader = `
+  #version 330
+
+  uniform sampler2D tex;
+
+  in vec2 fragTexCoord;
+
+  out vec4 outputColor;
+
+  void main() {
+    outputColor = texture(tex, fragTexCoord);
+  }
+` + "\x00"
+
 func init() {
   // GLFW event handling run on the main OS thread
   runtime.LockOSThread() // TODO check that that does
@@ -31,7 +115,7 @@ func main() {
 
   // Engine start - glfw
   if err := glfw.Init(); err != nil {
-    log.Fataln("Failed to initialize glfw:", err)
+    log.Fatalln("Failed to initialize glfw:", err)
   }
   defer glfw.Terminate();
 
@@ -39,7 +123,7 @@ func main() {
   glfw.WindowHint(glfw.ContextVersionMajor, 4)
   glfw.WindowHint(glfw.ContextVersionMinor, 1)
   glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-  glfw.WindowHint(glfw.OpenGLForwardCompativle, glfw.True)
+  glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 
   window, err := glfw.CreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Ships Trade", nil, nil)
   if err != nil {
@@ -48,6 +132,154 @@ func main() {
   window.MakeContextCurrent();
 
 
+  if err := gl.Init(); err != nil {
+    panic(err)
+  }
+
+  version := gl.GoStr(gl.GetString(gl.VERSION))
+  fmt.Println("OpenGL version", version)
+
+  // Configure the vertex and fragment shaders
+  program, err := newProgram(vertexShader, fragmentShader)
+  if err != nil {
+    panic(err)
+  }
+
+  gl.UseProgram(program)
+
+  // Projection camera model matrices my favourite!
+  projection := mgl32.Perspective(mgl32.DegToRad(45.0), float32(WINDOW_WIDTH) / WINDOW_HEIGHT, 0.1, 1000.0)
+  projectionUniform := gl.GetUniformLocation(program, gl.Str("projection\x00"))
+  gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
+
+  camera := mgl32.LookAtV(mgl32.Vec3{3, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
+  cameraUniform := gl.GetUniformLocation(program, gl.Str("camera\x00"))
+  gl.UniformMatrix4fv(cameraUniform, 1, false, &camera[0])
+
+  model := mgl32.Ident4()
+  modelUniform := gl.GetUniformLocation(program, gl.Str("model\x00"))
+  gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
+
+  textureUniform := gl.GetUniformLocation(program, gl.Str("tex\x00"))
+  gl.Uniform1i(textureUniform, 0)
+
+  gl.BindFragDataLocation(program, 0, gl.Str("outputColor\x00"))
+
+  // Load the texture
+  texture, err := newTexture("square.png")
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  // Configure the vertex data
+  var vao uint32
+  gl.GenVertexArrays(1, &vao)
+  gl.BindVertexArray(vao)
+
+  var vbo uint32
+  gl.GenBuffers(1, &vbo)
+  gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+  gl.BufferData(gl.ARRAY_BUFFER, len(cubeVertices) * 4, gl.Ptr(cubeVertices), gl.STATIC_DRAW)
+
+
+}
+
+func newTexture(file string) (uint32, error) {
+  imgFile, err := os.Open(file)
+  if err != nil {
+    return 0, fmt.Errorf("texture %q not found on disk: %v", file, err)
+  }
+
+  img, _, err := image.Decode(imgFile)
+  if err != nil {
+    return 0, err
+  }
+
+  rgba := image.NewRGBA(img.Bounds())
+  if rgba.Stride != rgba.Rect.Size().X*4 {
+    return 0, fmt.Errorf("unsopported stride")
+  }
+
+  draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
+
+  var texture uint32
+  gl.GenTextures(1, &texture)
+  gl.ActiveTexture(gl.TEXTURE0)
+  gl.BindTexture(gl.TEXTURE_2D, texture)
+  gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+  gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+  gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+  gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+  gl.TexImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    int32(rgba.Rect.Size().X),
+    int32(rgba.Rect.Size().Y),
+    0,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    gl.Ptr(rgba.Pix))
+
+  return texture, nil
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+  shader := gl.CreateShader(shaderType)
+
+  csources, free := gl.Strs(source)
+  gl.ShaderSource(shader, 1, csources, nil)
+  free()
+  gl.CompileShader(shader)
+
+  var status int32
+  gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+  if status == gl.FALSE {
+    var logLength int32
+    gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+    log := strings.Repeat("\x00", int(logLength + 1))
+    gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+    return 0, fmt.Errorf("failed to compiler: %v", source, log)
+  }
+
+  return shader, nil
+}
+
+func newProgram(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
+  vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+  if err != nil {
+    return 0, err
+  }
+
+  fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+  if err != nil {
+    return 0, err
+  }
+
+  program := gl.CreateProgram()
+
+  gl.AttachShader(program, vertexShader)
+  gl.AttachShader(program, fragmentShader)
+  gl.LinkProgram(program)
+
+  var status int32
+  gl.GetProgramiv(program, gl.LINK_STATUS, &status);
+  if status == gl.FALSE {
+    var logLength int32
+    gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+
+    log := strings.Repeat("\x00", int(logLength + 1))
+    gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
+
+    return 0, fmt.Errorf("failed to link program: %v", log)
+  }
+
+  gl.DeleteShader(vertexShader)
+  gl.DeleteShader(fragmentShader)
+
+  return program, nil
 }
 
 func init() {
